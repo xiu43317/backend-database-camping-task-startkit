8b8dcebcf4088fcd7c7ff89a09735fe7a35f9c2f
Rock <a123@RockMacBook-Air.local>
2024-12-07 05:16:54 +0800

    delete換行

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -38,7 +38,8 @@ VALUES
 -- WHERE email = 'starplatinum@hexschooltest.io';
 
 -- 1-3 刪除：刪除USER 資料表中，用 Email 找到透明人，並刪除該筆資料
-DELETE FROM  "USER"
+DELETE 
+FROM  "USER"
 WHERE email = 'opacity0@hexschooltest.io'
 
 -- 1-4 查詢：取得USER 資料表目前所有用戶數量（提示：使用count函式）
@@ -60,7 +61,7 @@ LIMIT 3
     -- 1. 名稱為 `7 堂組合包方案`，價格為`1,400` 元，堂數為`7`
     -- 2. 名稱為`14 堂組合包方案`，價格為`2,520` 元，堂數為`14`
     -- 3. 名稱為 `21 堂組合包方案`，價格為`4,800` 元，堂數為`21`
-INSERT into "CREDIT_PACKAGE" (name, credit_amount, price) VALUES
+INSERT INTO "CREDIT_PACKAGE" (name, credit_amount, price) VALUES
 ('7 堂組合包方案', 7, 1400),
 ('14 堂組合包方案', 14, 2520),
 ('21 堂組合包方案', 21, 4800)
@@ -69,7 +70,7 @@ INSERT into "CREDIT_PACKAGE" (name, credit_amount, price) VALUES
     -- 1. `王小明` 購買 `14 堂組合包方案`
     -- 2. `王小明` 購買 `21 堂組合包方案`
     -- 3. `好野人` 購買 `14 堂組合包方案`
-INSERT into "CREDIT_PURCHASE" (user_id, credit_package_id, purchASed_credits, price_paid) VALUES
+INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id, purchASed_credits, price_paid) VALUES
 ((SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io'),
   (SELECT id FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
   (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
@@ -94,7 +95,7 @@ INSERT into "CREDIT_PURCHASE" (user_id, credit_package_id, purchASed_credits, pr
     -- 1. 將用戶`李燕容`新增為教練，並且年資設定為2年（提示：使用`李燕容`的email ，取得 `李燕容` 的 `id` ）
     -- 2. 將用戶`肌肉棒子`新增為教練，並且年資設定為2年
     -- 3. 將用戶`Q太郎`新增為教練，並且年資設定為2年
-INSERT into "COACH" (user_id, experience_years) VALUES 
+INSERT INTO "COACH" (user_id, experience_years) VALUES 
 ((SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io'), 2),
 ((SELECT id FROM "USER" WHERE email = 'muscle@hexschooltest.io'), 2),
 ((SELECT id FROM "USER" WHERE email = 'starplatinum@hexschooltest.io'), 2);
@@ -103,7 +104,7 @@ INSERT into "COACH" (user_id, experience_years) VALUES
     -- 1. 所有教練都有 `重訓` 專長
     -- 2. 教練`肌肉棒子` 需要有 `瑜伽` 專長
     -- 3. 教練`Q太郎` 需要有 `有氧運動` 與 `復健訓練` 專長
-INSERT into "COACH_LINK_SKILL" (coach_id, skill_id) VALUES 
+INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id) VALUES 
 (
   (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io')),
   (SELECT id FROM "SKILL" WHERE name = '重訓')
@@ -146,7 +147,7 @@ WHERE user_id = (
     WHERE email = 'starplatinum@hexschooltest.io');
 
 -- 3-4 刪除：新增一個專長 空中瑜伽 至 SKILL 資料表，之後刪除此專長。
-INSERT into "SKILL" (name) VALUES ('空中瑜伽');
+INSERT INTO "SKILL" (name) VALUES ('空中瑜伽');
 DELETE FROM "SKILL" WHERE name = '空中瑜伽';
 --  ████████  █████   █    █   █ 
 --    █ █   ██    █  █     █   █ 
@@ -164,7 +165,7 @@ DELETE FROM "SKILL" WHERE name = '空中瑜伽';
     -- 5. 授課結束時間`end_at`設定為2024-11-25 16:00:00
     -- 6. 最大授課人數`max_participants` 設定為10
     -- 7. 授課連結設定`meeting_url`為 https://test-meeting.test.io
-INSERT into "COURSE" (user_id, skill_id, name, start_at, end_at, max_participants, meeting_url) VALUES
+INSERT INTO "COURSE" (user_id, skill_id, name, start_at, end_at, max_participants, meeting_url) VALUES
 ((SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io'),
   (SELECT id FROM "SKILL" WHERE name = '重訓'),
   '重訓基礎課',
@@ -191,7 +192,7 @@ INSERT into "COURSE" (user_id, skill_id, name, start_at, end_at, max_participant
         -- 1. 預約人設為 `好野人`
         -- 2. 預約時間`booking_at` 設為2024-11-24 16:00:00
         -- 3. 狀態`status` 設定為即將授課
-INSERT into "COURSE_BOOKING" (user_id, course_id, booking_at, status) VALUES
+INSERT INTO "COURSE_BOOKING" (user_id, course_id, booking_at, status) VALUES
 ((SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io'),
   (SELECT id FROM "COURSE" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io')),
   '2024-11-24 16:00:00',
@@ -212,7 +213,7 @@ AND course_id = (SELECT id FROM "COURSE" WHERE user_id = (SELECT id FROM "USER" 
     -- 1. 預約人設為`王小明`
     -- 2. 預約時間`booking_at` 設為2024-11-24 17:10:25
     -- 3. 狀態`status` 設定為即將授課
-INSERT into "COURSE_BOOKING" (user_id, course_id, booking_at, status) VALUES
+INSERT INTO "COURSE_BOOKING" (user_id, course_id, booking_at, status) VALUES
 ((SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io'),
   (SELECT id FROM "COURSE" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io')),
   '2024-11-24 17:10:25',
