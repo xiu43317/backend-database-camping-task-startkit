d90d094a0552faed7e420e3b3309cfebe563fb4e
Rock <a123@RockMacBook-Air.local>
2024-12-07 04:37:02 +0800

    修改大寫

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -26,7 +26,7 @@ VALUES
 
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
 UPDATE "USER"
-SET role = 'coach'
+SET role = 'COACH'
 WHERE email = 'lee2000@hexschooltest.io'
 OR email = 'muscle@hexschooltest.io'
 OR email = 'starplatinum@hexschooltest.io';
